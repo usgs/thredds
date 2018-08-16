a4a0744169295905f38967a8e100504347c58a23
wchen <wchen@usgs.gov>
2018-08-16 13:40:00 -0500

    Scheduled commit

--- cdm/src/main/java/ucar/nc2/ft2/coverage/simpgeometry/SimpleGeometryKitten.java
+++ cdm/src/main/java/ucar/nc2/ft2/coverage/simpgeometry/SimpleGeometryKitten.java
@@ -8,7 +8,7 @@ import ucar.nc2.ft2.coverage.simpgeometry.*;
  * A cat (the animal) trained in finding Simple Geometry.
  * 
  * A Simple Geometry Kitten can go find the beginning and end indicies of
- * a simple geometry X and Y within a variable. But first the Kitten needs a Variable to find it in.
+ * a simple geometry X and Y within a variable. But first the Kitten needs a dataset to find it in.
  * The kitten remembers previous adventures so if the kitten is tasked to find a Simple Geometry close to one before it
  * will find it faster.
  * 
@@ -17,13 +17,13 @@ import ucar.nc2.ft2.coverage.simpgeometry.*;
  */
 public class SimpleGeometryKitten {
 	
-	Variable cat_toy;
-	Variable node_count;
-	Variable part_node_count;
+	Variable cat_toy = null;
+	Variable node_count = null;
+	Variable part_node_count = null;
 	String geometry_type;
 	int past_index;
 	int previous_end, new_end;
-	int previous_begin, new_being;
+	int previous_begin, new_begin;
 	
 	
 	public int getBeginning(int index) {
@@ -50,25 +50,20 @@ public class SimpleGeometryKitten {
 			return previous_begin - 1;
 		}
 		
-		// Otherwise find it
+		// Otherwise find it!
 		
 		return new_end;
 	}
 	
 	/**
-	 * Call up a new Kitten, the Kitten must be given a Variable though.
+	 * Call up a new Kitten, the Kitten must be given a variable to look through though.
 	 * 
 	 * @param variable Variable to give it
 	 */
 	public SimpleGeometryKitten(Variable variable) {
-		cat_toy = variable;
 		
-		if(variable.findAttribute(CF.GEOMETRY_TYPE) != null)
-		{
-			geometry_type = variable.findAttribute(CF.GEOMETRY_TYPE).getStringValue();
-		}
-		
-		past_index = -3;
-		previous_end = -1;
+		past_index = -1;
+		previous_end = -1; new_end = -1;
+		previous_begin = -1; new_begin = -1;
 	}
 }
