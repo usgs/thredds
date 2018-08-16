1b215c51eac7b55bf24b794ac635008e61747715
wchen <wchen@usgs.gov>
2018-08-16 13:50:00 -0500

    Scheduled commit

--- cdm/src/main/java/ucar/nc2/ft2/coverage/simpgeometry/SimpleGeometryKitten.java
+++ cdm/src/main/java/ucar/nc2/ft2/coverage/simpgeometry/SimpleGeometryKitten.java
@@ -8,7 +8,7 @@ import ucar.nc2.ft2.coverage.simpgeometry.*;
  * A cat (the animal) trained in finding Simple Geometry.
  * 
  * A Simple Geometry Kitten can go find the beginning and end indicies of
- * a simple geometry X and Y within a variable. But first the Kitten needs a dataset to find it in.
+ * a simple geometry X and Y within a variable. But first the Kitten needs a few variables to find it in.
  * The kitten remembers previous adventures so if the kitten is tasked to find a Simple Geometry close to one before it
  * will find it faster.
  * 
@@ -17,14 +17,17 @@ import ucar.nc2.ft2.coverage.simpgeometry.*;
  */
 public class SimpleGeometryKitten {
 	
-	Variable cat_toy = null;
-	Variable node_count = null;
-	Variable part_node_count = null;
-	String geometry_type;
-	int past_index;
-	int previous_end, new_end;
-	int previous_begin, new_begin;
+	private Variable cat_toy = null;
+	private Variable node_count = null;
+	private Variable part_node_count = null;
+	private String geometry_type;
+	private int past_index;
+	private int previous_end, new_end;
+	private int previous_begin, new_begin;
 	
+	private int getNodeCount(int index) {
+		
+	}
 	
 	public int getBeginning(int index) {
 		
@@ -34,9 +37,13 @@ public class SimpleGeometryKitten {
 			return previous_end + 1;
 		}
 		
-		int new_beginning = -1;
+
 		
 		// Otherwise, find it!
+		int new_beginning = 0;
+		for(int i = 0; i < index; i++) {
+			new_beginning += getNodeCount(i);
+		}
 		
 		past_index = index;
 		return new_beginning;
@@ -51,16 +58,24 @@ public class SimpleGeometryKitten {
 		}
 		
 		// Otherwise find it!
+		int new_end = 0;
+		for(int i = 0; i < index + 1; i++) {
+			new_end += getNodeCount(i);
+		}
+		
+		new_end -= 1;
 		
 		return new_end;
 	}
 	
 	/**
-	 * Call up a new Kitten, the Kitten must be given a variable to look through though.
+	 * Call up a new Kitten, the Kitten must be given a few variables to look through though.
 	 * 
-	 * @param variable Variable to give it
+	 * @param variable Variable with all information concerning polygon point data
+	 * @param node_count Amount of nodes per geometry
+	 * @param variable part_node_count
 	 */
-	public SimpleGeometryKitten(Variable variable) {
+	public SimpleGeometryKitten(Variable variable, Variable node_count, Variable part_node_count) {
 		
 		past_index = -1;
 		previous_end = -1; new_end = -1;
