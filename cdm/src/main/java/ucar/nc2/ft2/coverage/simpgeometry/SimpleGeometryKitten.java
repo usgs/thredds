2ed105c6742d7bbfdc752b3fbbc2beeb45c91fe8
wchen <wchen@usgs.gov>
2018-08-16 13:00:01 -0500

    Scheduled commit

---
+++ cdm/src/main/java/ucar/nc2/ft2/coverage/simpgeometry/SimpleGeometryKitten.java
@@ -0,0 +1,36 @@
+package ucar.nc2.ft2.coverage.simpgeometry;
+
+import ucar.nc2.Variable;
+import ucar.nc2.constants.CF;
+
+/**
+ * A cat (the animal) made out of Simple Geometry.
+ * 
+ * A Simple Geometry Kitten can go find the beginning and end indicies of
+ * a simple geometry X and Y within a variable. But first the Kitten needs a Variable to find it in.
+ * 
+ * @author wchen
+ *
+ */
+public class SimpleGeometryKitten {
+	
+	Variable cat_toy;
+	
+	public int getBeing(int index) {
+		
+	}
+	
+	public int getEnd(int index) {
+		
+	}
+	
+	/**
+	 * Call up a new Kitten, the Kitten must be given a Variable though.
+	 * 
+	 * @param variable Variable to give it
+	 */
+	public SimpleGeometryKitten(Variable variable) {
+		cat_toy = variable;
+		cat_toy.findAttribute(CF.GEOMETRY);
+	}
+}
