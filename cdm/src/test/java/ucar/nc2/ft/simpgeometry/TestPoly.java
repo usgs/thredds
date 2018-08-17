99772ac33b7fc77c4119b2e12acd6e367decaf20
wchen <wchen@usgs.gov>
2018-08-17 13:20:01 -0500

    Scheduled commit

--- cdm/src/test/java/ucar/nc2/ft/simpgeometry/TestPoly.java
+++ cdm/src/test/java/ucar/nc2/ft/simpgeometry/TestPoly.java
@@ -17,6 +17,8 @@ public class TestPoly {
 	@Test
 	public void testMultiPolygons() {
 
+		double err = 0.0;
+		
 		NetcdfDataset data = null ;
 		try {
 			data = NetcdfDataset.openDataset(TestDir.cdmLocalTestDataDir + "dataset/SimpleGeos/hru_soil_moist_3hru_5timestep.nc");
@@ -29,27 +31,14 @@ public class TestPoly {
 		Polygon poly = rdr.readPolygon("hru_soil_moist", 0);
 		Polygon poly_2 = rdr.readPolygon("hru_soil_moist", 1);
 		Polygon poly_3 = rdr.readPolygon("hru_soil_moist", 2);
-				
-		while(poly != null)
-		{
-			
-				List<CFPoint> pts = poly.getPoints();
-				Iterator<CFPoint> itr = pts.iterator();
-				int i = 0;
-				//while(itr.hasNext())
-				//{
-				//	i++;
-				//	CFPoint pt = itr.next();
-				//	println("( " + pt.getX() + " , " + pt.getY() + " )");
-				//}
-
-				poly = poly.getNext();
-		}
 		
 		Assert.assertEquals(6233, poly.getPoints().size());
 		Assert.assertEquals(5, poly_2.getPoints().size());
 		Assert.assertEquals(6033, poly_2.getNext().getPoints().size());
 		Assert.assertEquals(5135, poly_3.getPoints().size());
+		
+		// Test data retrieval
+		Assert.assertEquals(1.28, poly.getData().getDouble(2), err);
 	}
 	
 }
