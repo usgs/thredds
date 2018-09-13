36a4d83ad8c78101cb4940b71c460efc119864d2
skaymen <skaymen@usgs.gov>
2018-09-13 13:08:57 -0500

    fix schema server

--- tds/src/main/java/thredds/server/wfs/WFSDescribeFeatureTypeWriter.java
+++ tds/src/main/java/thredds/server/wfs/WFSDescribeFeatureTypeWriter.java
@@ -20,10 +20,10 @@ public class WFSDescribeFeatureTypeWriter {
 
     public void startXML() {
         fileOutput += "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
-        fileOutput += "<schema xmlns:ms=\"http://mapserver.gis.umn.edu/mapserver\" " +
+        fileOutput += "<schema xmlns:ms=\"" + server + "\" " +
                 "xmlns:ogc=\"http://www.opengis.net/ogc\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" " +
                 "xmlns=\"http://www.w3.org/2001/XMLSchema\" xmlns:gml=\"http://www.opengis.net/gml\" " +
-                "targetNamespace=\"http://mapserver.gis.umn.edu/mapserver\" elementFormDefault=\"qualified\" " +
+                "targetNamespace=\"" + server + "\" elementFormDefault=\"qualified\" " +
                 "version=\"0.1\">";
         fileOutput += "<import namespace=\"http://www.opengis.net/gml\" " +
                 "schemaLocation=\"http://schemas.opengis.net/gml/2.1.2/feature.xsd\"/>";
